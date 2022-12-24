f7fa553a42bb8abc4446b8a808c34f9f5667a96a
quisethecoder <marquise_dawkins@aol.com>
2022-12-24 17:55:40 -0500

    js html edit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const dropList = document.querySelectorAll(".drop-list select");
+
+for(let i = 0; i < dropList.length; i++){
+    for(currency_code in country_code){
+        let optionTag = "<option value='${currency_code}'>USD</option>"
+    }
+}
