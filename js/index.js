6368b3a47e95fd11a6a7d390e47f886eccebde5f
quisethecoder <marquise_dawkins@aol.com>
2022-12-24 19:10:27 -0500

    js edit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const dropList = document.querySelectorAll(".drop-list select");
 
 for(let i = 0; i < dropList.length; i++){
-    for(currency_code in country_code){
-        let optionTag = "<option value='${currency_code}'>USD</option>"
+    for(currency_code in country_list){
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "NPR" ? "selected" : "";
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
 }
