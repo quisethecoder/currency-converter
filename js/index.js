661aa4a6b11411489c9e2af6f57559ad2c76c707
quisethecoder <marquise_dawkins@aol.com>
2022-12-24 19:27:20 -0500

    before api

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const dropList = document.querySelectorAll(".drop-list select");
+const dropList = document.querySelectorAll(".drop-list select"),
+getButton = document.querySelector("form button");
 
 for(let i = 0; i < dropList.length; i++){
     for(currency_code in country_list){
@@ -6,4 +7,19 @@ for(let i = 0; i < dropList.length; i++){
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
+}
+
+getButton.addEventListener("click", e => {
+    e.preventDefault(); 
+    getExchangeRate();
+});
+
+function getExchangeRate(){
+    const amount = document.querySelector(".amount input");
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    
 }
